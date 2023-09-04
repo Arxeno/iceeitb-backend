8384963a388d94a076094da55e4d58951a3960bb
Arxeno <rmfbkw04@gmail.com>
2023-09-05 01:24:14 +0700

    fix: static file

--- icee_backend2/urls.py
+++ icee_backend2/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 admin.site.site_header = 'ICEE ITB 2024 CMS'
 admin.site.site_title = 'ICEE 2024'
@@ -23,4 +25,4 @@ admin.site.site_title = 'ICEE 2024'
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('register.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
