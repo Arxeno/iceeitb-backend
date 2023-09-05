fdeb5a4c8090630758e3f70d95b79484b1eab309
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-09-05 14:26:00 +0700

    add cors

--- icee_backend2/settings.py
+++ icee_backend2/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'register',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'icee_backend2.urls'
