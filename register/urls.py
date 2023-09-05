2b0688e4dac1ead437348d6ccb78e1d974cfc3fe
Arxeno <rmfbkw04@gmail.com>
2023-09-06 02:42:04 +0700

    fix: create separate endpoint

--- register/urls.py
+++ register/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('register', views.register_multipart),
+    path('register/team/<str:team_id>', views.register_team_payment),
+    path('register/member/ktm/<str:member_id>', views.register_member_ktm),
+    path('register/member/aktif/<str:member_id>', views.register_member_active),
+    path('register/member/3x4/<str:member_id>', views.register_member_3x4),
+    path('register/member/twibbon/<str:member_id>',
+         views.register_member_twibbon),
     path('uploads/<str:team_name>/<str:filename>', views.get_uploads),
     # path('upload_drive', views.upload_to_google_drive)
 ]
--- register/views.py
+++ register/views.py
@@ -12,33 +12,33 @@ import base64
 # Create your views here.
 
 
-@csrf_exempt
-def register_team(request):
-    if request.method == 'POST':
-        body_bytes = request.body
-        body_unicode = body_bytes.decode('utf-8')
+# @csrf_exempt
+# def register_team(request):
+#     if request.method == 'POST':
+#         body_bytes = request.body
+#         body_unicode = body_bytes.decode('utf-8')
 
-        try:
-            request_data = json.loads(body_unicode)
-        except json.JSONDecodeError:
-            return HttpResponseBadRequest('BAD REQUEST')
+#         try:
+#             request_data = json.loads(body_unicode)
+#         except json.JSONDecodeError:
+#             return HttpResponseBadRequest('BAD REQUEST')
 
-        # new_team = Team(team_name=request_data.teamName, total_team_members=request_data.totalTeamMembers, leader_name=request_data.leaderName,
-        #                 leader_university=request_data.leaderUniversity, leader_major=request_data.leaderMajor, leader_whatsapp_number=request_data.leaderWANumber, leader_email=request_data.leaderEmail, leader_address=request_data.leaderAddress, leader_student_id=request_data.leaderStudentID, leader_active_student_proof=request_data.leaderActiveStudentProof, leader_3x4_photo=request_data.leader3x4Photo, member_1_name=request_data.members[0].Name, member_1_university=request_data.members[0].University, member_1_major=request_data.members[0].Major, member_1_whatsapp_number=request_data.members[0].WANumber, member_1_email=request_data.members[0].Email, member_1_address=request_data.members[0].Address, member_1_student_id=request_data.members[0].StudentID, member_1_active_student_proof=request_data.members[0].ActiveStudentProof, member_1_3x4_photo=request_data.members[0].3x4Photo, member_2_name=request_data.member2Name, member_2_university=request_data.member2University, member_2_major=request_data.member2Major, member_2_whatsapp_number=request_data.member2WANumber, member_2_email=request_data.member2Email, member_2_address=request_data.member2Address, member_2_student_id=request_data.member2StudentID, member_2_active_student_proof=request_data.member2ActiveStudentProof, member_2_3x4_photo=request_data.member23x4Photo, member_3_name=request_data.member3Name, member_3_university=request_data.member3University, member_3_major=request_data.member3Major, member_3_whatsapp_number=request_data.member3WANumber, member_3_email=request_data.member3Email, member_3_address=request_data.member3Address, member_3_student_id=request_data.member3StudentID, member_3_active_student_proof=request_data.member3ActiveStudentProof, member_3_3x4_photo=request_data.member33x4Photo, payment_total=request_data.paymentTotal, referral_code=request_data.referralCode, payment_methods=request_data.paymentMethods, payment_proof=request_data.paymentProof)
+#         # new_team = Team(team_name=request_data.teamName, total_team_members=request_data.totalTeamMembers, leader_name=request_data.leaderName,
+#         #                 leader_university=request_data.leaderUniversity, leader_major=request_data.leaderMajor, leader_whatsapp_number=request_data.leaderWANumber, leader_email=request_data.leaderEmail, leader_address=request_data.leaderAddress, leader_student_id=request_data.leaderStudentID, leader_active_student_proof=request_data.leaderActiveStudentProof, leader_3x4_photo=request_data.leader3x4Photo, member_1_name=request_data.members[0].Name, member_1_university=request_data.members[0].University, member_1_major=request_data.members[0].Major, member_1_whatsapp_number=request_data.members[0].WANumber, member_1_email=request_data.members[0].Email, member_1_address=request_data.members[0].Address, member_1_student_id=request_data.members[0].StudentID, member_1_active_student_proof=request_data.members[0].ActiveStudentProof, member_1_3x4_photo=request_data.members[0].3x4Photo, member_2_name=request_data.member2Name, member_2_university=request_data.member2University, member_2_major=request_data.member2Major, member_2_whatsapp_number=request_data.member2WANumber, member_2_email=request_data.member2Email, member_2_address=request_data.member2Address, member_2_student_id=request_data.member2StudentID, member_2_active_student_proof=request_data.member2ActiveStudentProof, member_2_3x4_photo=request_data.member23x4Photo, member_3_name=request_data.member3Name, member_3_university=request_data.member3University, member_3_major=request_data.member3Major, member_3_whatsapp_number=request_data.member3WANumber, member_3_email=request_data.member3Email, member_3_address=request_data.member3Address, member_3_student_id=request_data.member3StudentID, member_3_active_student_proof=request_data.member3ActiveStudentProof, member_3_3x4_photo=request_data.member33x4Photo, payment_total=request_data.paymentTotal, referral_code=request_data.referralCode, payment_methods=request_data.paymentMethods, payment_proof=request_data.paymentProof)
 
-        new_team = Team(team_name=request_data["teamName"], payment_total=request_data["totalPayment"], referral_code=request_data["referralCode"],
-                        payment_methods=request_data["paymentMethod"], payment_proof=request_data["paymentProof"])
-        # new_team.save()
+#         new_team = Team(team_name=request_data["teamName"], payment_total=request_data["totalPayment"], referral_code=request_data["referralCode"],
+#                         payment_methods=request_data["paymentMethod"], payment_proof=request_data["paymentProof"])
+#         # new_team.save()
 
-        for member in request_data['members']:
-            new_member = Member(name=member['name'], team_id=new_team, role=member['role'], university=member['university'], major=member['major'], whatsapp_number=member['WANumber'], email=member['email'],
-                                address=member['address'], student_id=member['studentID'], active_student_proof=member['activeStudentProof'], photo_3x4=member['photo3x4'], photo_twibbon=member['photoTwibbon'])
-            # new_member.save()
+#         for member in request_data['members']:
+#             new_member = Member(name=member['name'], team_id=new_team, role=member['role'], university=member['university'], major=member['major'], whatsapp_number=member['WANumber'], email=member['email'],
+#                                 address=member['address'], student_id=member['studentID'], active_student_proof=member['activeStudentProof'], photo_3x4=member['photo3x4'], photo_twibbon=member['photoTwibbon'])
+#             # new_member.save()
 
-        # return JsonResponse(request_data)
-        return HttpResponse('Success!')
-    else:
-        return HttpResponseBadRequest('ONLY POST')
+#         # return JsonResponse(request_data)
+#         return HttpResponse('Success!')
+#     else:
+#         return HttpResponseBadRequest('ONLY POST')
 
 
 @csrf_exempt
@@ -113,20 +113,21 @@ def register_multipart(request):
 
         # payment
         print('three')
-        payment_proof = json.loads(request.POST['paymentProof'])
+        # payment_proof = json.loads(request.POST['paymentProof'])
         print('four')
         # print(payment_proof['base64'])
-        payment_content = ContentFile(
-            base64.b64decode(payment_proof['base64']))
+        # payment_content = ContentFile(
+        #     base64.b64decode(payment_proof['base64']))
         # payment_content.name = f'payment.{payment_proof["ext"]}'
-        payment_content.content_type = payment_proof['contentType']
+        # payment_content.content_type = payment_proof['contentType']
         print('five')
 
         new_team = Team(team_name=request_data["teamName"], competition=competition, payment_total=request_data["totalPayment"], referral_code=request_data["referralCode"],
                         payment_methods=request_data["paymentMethod"])
         new_team.save()
-        new_team.payment_proof.save(
-            f'payment.{payment_proof["ext"]}', payment_content)
+        # new_team.payment_proof.save(
+        #     f'payment.{payment_proof["ext"]}', payment_content)
+        ids = {"teamId": new_team.team_id, "memberIds": []}
 
         for i in range(len(members)):
             if (i == 0):
@@ -135,39 +136,43 @@ def register_multipart(request):
                 key = f'member{i}'
 
             # student id
-            member_student_id = json.loads(request.POST[f'{key}KTM'])
-            student_id_content = ContentFile(
-                base64.b64decode(member_student_id['base64']))
-            student_id_content.content_type = member_student_id['contentType']
+            # member_student_id = json.loads(request.POST[f'{key}KTM'])
+            # student_id_content = ContentFile(
+            #     base64.b64decode(member_student_id['base64']))
+            # student_id_content.content_type = member_student_id['contentType']
+
             # active student proof
-            member_active_student_proof = json.loads(
-                request.POST[f'{key}Active'])
-            active_student_proof_content = ContentFile(
-                base64.b64decode(member_active_student_proof['base64']))
-            active_student_proof_content.content_type = member_active_student_proof[
-                'contentType']
+            # member_active_student_proof = json.loads(
+            #     request.POST[f'{key}Active'])
+            # active_student_proof_content = ContentFile(
+            #     base64.b64decode(member_active_student_proof['base64']))
+            # active_student_proof_content.content_type = member_active_student_proof[
+            #     'contentType']
+
             # photo 3x4
-            member_photo_3x4 = json.loads(request.POST[f'{key}3x4'])
-            photo_3x4_content = ContentFile(
-                base64.b64decode(member_photo_3x4['base64']))
-            photo_3x4_content.content_type = member_photo_3x4['contentType']
+            # member_photo_3x4 = json.loads(request.POST[f'{key}3x4'])
+            # photo_3x4_content = ContentFile(
+            #     base64.b64decode(member_photo_3x4['base64']))
+            # photo_3x4_content.content_type = member_photo_3x4['contentType']
+
             # twibbon
-            member_photo_twibbon = json.loads(request.POST[f'{key}Twibbon'])
-            photo_twibbon_content = ContentFile(
-                base64.b64decode(member_photo_twibbon['base64']))
-            photo_twibbon_content.content_type = member_photo_twibbon['contentType']
+            # member_photo_twibbon = json.loads(request.POST[f'{key}Twibbon'])
+            # photo_twibbon_content = ContentFile(
+            #     base64.b64decode(member_photo_twibbon['base64']))
+            # photo_twibbon_content.content_type = member_photo_twibbon['contentType']
 
             new_member = Member(name=members[i]['name'], team_id=new_team, role=members[i]['role'], university=members[i]['university'], major=members[i]['major'], whatsapp_number=members[i]['WANumber'], email=members[i]['email'],
                                 address=members[i]['address'])
 
-            content_files = [student_id_content, active_student_proof_content,
-                             photo_3x4_content, photo_twibbon_content]
-            exts = [member_student_id['ext'], member_active_student_proof['ext'],
-                    member_photo_3x4['ext'], member_photo_twibbon['ext']]
-            new_member.save_image_files(content_files, exts)
+            # content_files = [student_id_content, active_student_proof_content,
+            #                  photo_3x4_content, photo_twibbon_content]
+            # exts = [member_student_id['ext'], member_active_student_proof['ext'],
+            #         member_photo_3x4['ext'], member_photo_twibbon['ext']]
+            # new_member.save_image_files(content_files, exts)
             new_member.save()
+            ids['memberIds'].append(new_member.member_id)
 
-        return JsonResponse({"message": f'Sukses mendaftarkan tim!', "statusCode": 201}, status=201)
+        return JsonResponse({"message": f'Sukses mendaftarkan tim!', "statusCode": 201, "ids": ids}, status=201)
     else:
         return JsonResponse({"message": 'ONLY POST', "statusCode": 400}, status=400)
 
@@ -204,16 +209,76 @@ def get_uploads(request, team_name, filename):
     # return FileResponse(file)
 
 
-# @csrf_exempt
-# def upload_to_google_drive(request):
-#     if request.method == 'POST' and request.FILES:
-#         uploaded_file = request.FILES['file']
-#         file_title = uploaded_file.name
+@csrf_exempt
+def register_team_payment(request, team_id):
+    if request.method == 'POST':
+        try:
+            payment_proof = request.FILES.get('paymentProof')
 
-#         # Upload the file to Google Drive
-#         gdrive_file = drive.CreateFile({'title': file_title})
-#         gdrive_file.Upload()
+            registered_team = Team.objects.get(team_id=team_id)
+            registered_team.payment_proof.save(payment_proof)
+            return JsonResponse({"message": "Success upload team images", "statusCode": 201}, status=201)
+        except:
+            return JsonResponse({"message": "Cannot find team", "statusCode": 400}, status=400)
+    else:
+        return JsonResponse({"message": "ONLY POST", "statusCode": 400}, status=400)
 
-#         return JsonResponse({'message': f'File "{file_title}" uploaded to Google Drive successfully.'})
 
-#     return HttpResponseBadRequest('ERROR')
+@csrf_exempt
+def register_member_ktm(request, member_id):
+    if request.methods == 'POST':
+        try:
+            ktm = request.FILES.get('paymentProof')
+
+            registered_member = Member.objects.get(member_id=member_id)
+            registered_member.student_id.save(ktm)
+            return JsonResponse({"message": "Success upload member images", "statusCode": 201}, status=201)
+        except:
+            return JsonResponse({"message": "Cannot find member", "statusCode": 400}, status=400)
+    else:
+        return JsonResponse({"message": "ONLY POST", "statusCode": 400}, status=400)
+
+
+@csrf_exempt
+def register_member_active(request, member_id):
+    if request.methods == 'POST':
+        try:
+            aktif = request.FILES.get('paymentProof')
+
+            registered_member = Member.objects.get(member_id=member_id)
+            registered_member.active_student_proof.save(aktif)
+            return JsonResponse({"message": "Success upload member images", "statusCode": 201}, status=201)
+        except:
+            return JsonResponse({"message": "Cannot find member", "statusCode": 400}, status=400)
+    else:
+        return JsonResponse({"message": "ONLY POST", "statusCode": 400}, status=400)
+
+
+@csrf_exempt
+def register_member_3x4(request, member_id):
+    if request.methods == 'POST':
+        try:
+            photo3x4 = request.FILES.get('paymentProof')
+
+            registered_member = Member.objects.get(member_id=member_id)
+            registered_member.photo_3x4.save(photo3x4)
+            return JsonResponse({"message": "Success upload member images", "statusCode": 201}, status=201)
+        except:
+            return JsonResponse({"message": "Cannot find member", "statusCode": 400}, status=400)
+    else:
+        return JsonResponse({"message": "ONLY POST", "statusCode": 400}, status=400)
+
+
+@csrf_exempt
+def register_member_twibbon(request, member_id):
+    if request.methods == 'POST':
+        try:
+            twibbon = request.FILES.get('paymentProof')
+
+            registered_member = Member.objects.get(member_id=member_id)
+            registered_member.student_id.save(twibbon)
+            return JsonResponse({"message": "Success upload member images", "statusCode": 201}, status=201)
+        except:
+            return JsonResponse({"message": "Cannot find member", "statusCode": 400}, status=400)
+    else:
+        return JsonResponse({"message": "ONLY POST", "statusCode": 400}, status=400)
